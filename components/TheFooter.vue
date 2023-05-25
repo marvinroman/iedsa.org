ba2bf707ebe6c315c1799df7c87f6c85bf7a38ca
Marvin Roman <marvin@colab.coop>
2023-05-25 09:52:29 -0700

    feat: :sparkles: footer completed

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,26 +1,52 @@
 <template>
-  <div>
-    <ul>
-      <li>Copyright - {{ date.getFullYear() }}</li>
-    </ul>
-  </div>
+  <v-footer elevation="4">
+    <v-card
+      flat
+      class="white--text text-center align-self-md-stretch"
+      width="100%"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="(social, index) in socials"
+          :key="index"
+          :href="social.link"
+          class="mx-4 white--text"
+          target="_blank"
+          rel="noopener noreferrer"
+          icon
+          link
+        >
+          <v-icon size="24px">{{ social.icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        <v-icon>mdi-copy-left</v-icon>&nbsp;{{ new Date().getFullYear() }} —
+        <strong>{{ $config.organization.long_name }}</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
-<script setup lang="ts">
-const date = ref(new Date())
-</script>
-
-<style scoped lang="scss">
-div {
-  background-color: black;
-  color: #fff;
-  padding: 0.2rem;
-
-  ul {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+<script>
+export default {
+  components: {},
+  data: () => ({
+    socials: [
+      {
+        link: 'https://www.facebook.com/InlandEmpireDSA',
+        icon: 'mdi-facebook',
+      },
+      { link: 'https://twitter.com/InlandEmpireDSA', icon: 'mdi-twitter' },
+      {
+        link: 'https://www.instagram.com/InlandEmpireDSA',
+        icon: 'mdi-instagram',
+      },
+      { link: 'https://twitch.tv/inlandempiredsa', icon: 'mdi-twitch' },
+      { link: 'mailto:inlanddsa@gmail.com', icon: 'mdi-email-outline' },
+    ],
+  }),
 }
-</style>
+</script>
