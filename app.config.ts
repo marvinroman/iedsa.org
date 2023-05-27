bf0ba0a4b07d96c89c3126cb8bc5a9b6eb8ce602
Marvin Roman <marvin@colab.coop>
2023-05-27 02:54:13 -0700

    chore: :building_construction: moved socials and menu to config

--- app.config.ts
+++ app.config.ts
@@ -2,4 +2,55 @@ export default defineAppConfig({
   long_title: 'Inland Empire Democratic Socialists of America',
   medium_title: 'Inland Empire Democratic Socialists',
   short_title: 'Inland Empire DSA',
+  socials: [
+    {
+      link: 'https://www.facebook.com/InlandEmpireDSA',
+      icon: 'mdi-facebook',
+    },
+    { link: 'https://twitter.com/InlandEmpireDSA', icon: 'mdi-twitter' },
+    {
+      link: 'https://www.instagram.com/InlandEmpireDSA',
+      icon: 'mdi-instagram',
+    },
+    { link: 'https://twitch.tv/inlandempiredsa', icon: 'mdi-twitch' },
+    { link: 'mailto:inlanddsa@gmail.com', icon: 'mdi-email-outline' },
+  ],
+  menu: [
+    {
+      name: 'Home',
+      icon: 'mdi-home',
+      path: '/',
+    },
+    {
+      name: 'Branches',
+      icon: 'mdi-map-marker-radius-outline',
+      path: '/branches',
+    },
+    {
+      name: 'News',
+      icon: 'mdi-newspaper-variant-multiple-outline',
+      items: [
+        {
+          name: 'Articles',
+          icon: 'mdi-newspaper-variant-outline',
+          path: '/articles',
+        },
+        {
+          name: 'Statements',
+          icon: 'mdi-bullhorn-variant-outline',
+          path: '/statements',
+        },
+      ],
+    },
+    {
+      name: 'Calendar',
+      icon: 'mdi-calendar-month-outline',
+      path: 'https://calendar.iedsa.org',
+    },
+    {
+      name: 'Join',
+      icon: 'mdi-handshake-outline',
+      path: 'https://act.dsausa.org/donate/membership',
+    },
+  ],
 })
